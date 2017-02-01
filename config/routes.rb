88ac49386ce9e0e65e52c5ba063b0394de340c92
codecademy <ccuser@codecademy.com>
2017-02-01 12:35:15 -0500

    Added bio page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#welcome'
   get 'portfolio' =>'portfolio#portfolio'
+  get 'about' => 'about#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
