36c0415ef5f36e28d6b43221abfe8adc7d8f2e5c
codecademy <ccuser@codecademy.com>
2017-01-31 21:43:53 -0500

    Small jquery adds

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,11 +9,11 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
 		jQuery(document).ready(function($) {
 			$('.my-slider').unslider();
 		});
